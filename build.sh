7d913a5709992621eab8aff367705334483c7683
7feilee <7feilee@gmail.com>
2023-04-23 16:22:47 +0200

    add protoc plugin

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 protoc -I proto/ --go_out=internal/proto --go_opt=paths=source_relative --go-grpc_out=internal/proto --go-grpc_opt=paths=source_relative proto/script_verifier.proto
